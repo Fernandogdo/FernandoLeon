8b389983e2fb4bcd5883cd158ad6c37fcba7337d
fernandogdo <fernandojaviergdo@gmail.com>
2025-03-05 00:39:42 -0500

    app actualizada

---
+++ src/app/services/products-service/product.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+import { ProductService } from './product.service';
+import { provideHttpClient } from '@angular/common/http';
+
+describe('ProductService', () => {
+  let service: ProductService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(), 
+        ProductService
+      ]
+    });
+    service = TestBed.inject(ProductService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
