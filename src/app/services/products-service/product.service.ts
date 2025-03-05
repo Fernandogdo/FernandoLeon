269e80d57202b68f176fd8e9753002d7d7ff0833
fernandogdo <fernandojaviergdo@gmail.com>
2025-03-05 01:26:13 -0500

    app actualizada

--- src/app/services/products-service/product.service.ts
+++ src/app/services/products-service/product.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../../models/product.mode';
-import { environment } from '../../../environments/environment'; // Importar environment
+import { environment } from '../../../environments/environment'; 
 
 
 @Injectable({
